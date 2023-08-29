e62533305a56197e381f8be61f59b5d4655943b2
spookydens <redouflsser@gmail.com>
2023-08-29 02:38:12 +0200

    improved
    added:
    - Sliding
    - Crouching
    - sens setting
    -Enemy (with Ai)
    -more Layers
    
    changed:
    -arrange the files
    
    removed:
    - Old Movement Scripts

---
+++ Dying Worlds/Assets/Scripts/Movement/PlayerCam.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    public float senX;
+    public float senY;
+
+    public Transform orientation;
+    internal bool whatIsGround;
+    float xRotation;
+    float yRotation;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * senX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * senY;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // Rotate Cam and Orientation
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+}
